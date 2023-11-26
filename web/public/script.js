cfa8287227f29add102ea63b2d98e6f3348a2415
AndreasVJ <andreas.vj@icloud.com>
2023-11-26 13:23:28 +0100

    correct version

--- web/public/script.js
+++ web/public/script.js
@@ -1,81 +1,49 @@
 import * as THREE from "three"
-import { collection, getDocs, orderBy } from "firebase/firestore"
+import {collection, getDocs } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-firestore.js"
 import { db } from "./app.js"
 
-
-const button2D = document.getElementById("button2D")
-const button3D = document.getElementById("button3D")
-const container2D = document.getElementById("container2D")
 const container3D = document.getElementById("container3D")
 
-button2D.onclick = () => {
-    container2D.style.display = "block"
-    container3D.style.display = "none"
-}
-
-button3D.onclick = () => {
-    container2D.style.display = "none"
-    container3D.style.display = "block"
-}
-
-const ctx = document.getElementById('myChart')
-new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: [1, 2, 3, 4, 5, 6, 7],
-        datasets: [{
-            // label: 'My First Dataset',
-            data: [65, 59, 80, 81, 56, 55, 40],
-            // borderColor: 'rgb(75, 192, 192)',
-            // tension: 0.1
-        }]
-    },
-    options: {
-        animation: false,
-        transitions: {
-            activate: {
-                animation: {
-                    duration: 0
-                }
-            }
-        }
-        // scales: {
-        //     y: {
-        //         beginAtZero: true
-        //     }
-        // }
-    }
-})
-
+const scene = new THREE.Scene()
+let mesh = createMesh([1, 1, 1, 1, 1, 1, 1, 1, 1], 0)
+scene.add(mesh)
 
 const sensorDataList = document.getElementById("sensorDataList")
 const sensorData = new Map()
 
+const querySnapshot = await getDocs(collection(db, "sensorData"))
 
-const querySnapshot = await getDocs(collection(db, "sensorData"), orderBy("created_at", "desc"))
+const sensorDataIDArray = []
 querySnapshot.forEach((doc) => {
     sensorData[doc.id] = doc.data().sensors
+    sensorDataIDArray.push(doc.id)
+})
+for (const id of sensorDataIDArray.reverse()) {
     const li = document.createElement("li")
-    const date = new Date(doc.id*1000)
-    li.innerText = date.toLocaleString('no-NO', { timeZone: 'Europe/Oslo' })
-    li.setAttribute("id", doc.id)
+    const date = new Date(id*1000)
+    li.innerText = date.toLocaleString('no-NO', {
+        timeZone: 'Europe/Oslo'
+    })
+    li.setAttribute("id", id)
     li.onclick = () => {
-        console.log(sensorData[li.id])
         scene.remove(mesh)
-        mesh = createMesh(scene, sensorData[li.id], mesh.rotation.y)
+        mesh = createMesh(sensorData[id], mesh.rotation.y)
+        scene.add(mesh)
     }
     sensorDataList.appendChild(li)
-})
+}
 
 
-const scene = new THREE.Scene()
+
 const camera = new THREE.PerspectiveCamera(75, 7 / 4, 0.1, 1000)
-
-let mesh = createMesh(scene, [1, 1, 1, 1, 1, 1, 1, 1, 1], 0)
 
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(700, 400)
 container3D.appendChild(renderer.domElement)
+
+// const axesHelper = new THREE.AxesHelper(5)
+// scene.add(axesHelper)
+
 
 const pg = new THREE.PlaneGeometry(100, 100)
 const pm = new THREE.MeshPhongMaterial({color: 0x0000ff, side: THREE.DoubleSide})
@@ -89,7 +57,7 @@ light.position.set(6, 10, 2)
 scene.add(light)
 
 
-function createMesh(scene, h, ry) {
+function createMesh(h, ry) {
     const d = 8
     const points = [
         d, h[0], d,
@@ -126,15 +94,20 @@ function createMesh(scene, h, ry) {
     ]
 
     const bg = new THREE.BufferGeometry()
-    const positionAttribute = new THREE.BufferAttribute(new Float32Array(points), 3)
+    const positionAttribute = new THREE.BufferAttribute(
+        new Float32Array(points), 
+        3
+    )
     bg.setAttribute('position', positionAttribute)
 
-    const bm = new THREE.MeshPhongMaterial({ color: 0x00ff00, side: THREE.DoubleSide })
+    const bm = new THREE.MeshPhongMaterial({ 
+        color: 0x00ff00, 
+        side: THREE.DoubleSide 
+    })
     bg.computeVertexNormals()
     const mesh = new THREE.Mesh(bg, bm)
     mesh.rotation.y = ry
 
-    scene.add(mesh)
     return mesh
 }
 
